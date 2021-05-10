a12442976570545701a451c3c760aeb66b65480a
Andrea Pferscher <andrea.pferscher@ist.tugraz.at>
2021-05-10 07:00:06 +0200

    increase usabilty of main file

--- ble_learning.py
+++ ble_learning.py
@@ -1,3 +1,4 @@
+import sys
 from BLESUL import BLESUL
 from FailSafeLearning.StatePrefixEqOracleFailSafe import StatePrefixOracleFailSafe
 from FailSafeLearning.FailSafeCacheSUL import FailSafeCacheSUL
@@ -5,18 +6,13 @@ from aalpy.learning_algs import run_Lstar
 from aalpy.utils import visualize_automaton
 from util import print_error_info
 
-serial_port = '/dev/tty.usbmodem141101'
-#advertiser_address = 'CC:78:AB:71:5C:85' # TI LAUNCHXL-CC2650
-#advertiser_address = '0C:61:CF:A0:95:8F' # TI LAUNCHXL-CC264CR2
-#advertiser_address = 'f6:a8:63:cd:b7:d4' # dwm 1001
-#advertiser_address = 'dc:a6:32:4b:59:61' # raspberry pi 4
-advertiser_address = '00:A0:50:00:00:03' # Cypress CY8CPROTO-063
+args_len = len(sys.argv) - 1
 
-#file = 'TI-LAUNCHXL-CC2650'
-#file = 'TI-LAUNCHXL-CC264CR2-without-length-request'
-#file = 'dwm-1001'
-#file = 'raspberrypi-4'
-file = 'Cypress-CY8CPROTO-063'
+if args_len < 2:
+    sys.exit("Too few arguments provided.\nUsage: python3 ble_learning.py 'serial_port' 'advertiser_address'")
+
+serial_port = sys.argv[1]
+advertiser_address = sys.argv[2]
 
 ble_sul = BLESUL(serial_port, advertiser_address)
 
@@ -26,8 +22,6 @@ sul = FailSafeCacheSUL(ble_sul)
 # define the input alphabet
 alphabet = ['scan_req', 'connection_req', 'length_req', 'length_rsp',  'feature_rsp', 'version_req', 'mtu_req', 'pairing_req', 'feature_req']
 
-#alphabet = ['scan_req', 'connection_req', 'feature_req', 'length_rsp',  'feature_rsp', 'version_req', 'mtu_req', 'pairing_req']
-
 
 # define a equivalence oracle
 eq_oracle = StatePrefixOracleFailSafe(alphabet, sul, walks_per_state=10, walk_len=10)
@@ -35,8 +29,9 @@ eq_oracle = StatePrefixOracleFailSafe(alphabet, sul, walks_per_state=10, walk_le
 # run the learning algorithm
 # internal caching is disabled, since we require an error handling for possible non-deterministic behavior
 learned_model = run_Lstar(alphabet, sul, eq_oracle, automaton_type='mealy',cache_and_non_det_check=False)
-# visualize the automaton
 
+# prints number of connection and non-deterministic errors
 print_error_info(ble_sul, sul)
 
-visualize_automaton(learned_model, path=file, file_type='dot')
+# visualize the automaton
+visualize_automaton(learned_model, file_type='dot')
